4eeadd5347c97b0885fb6d2a730ec2a6ec7d4306
luciano <luciano.delapena@gmail.com>
2025-09-10 11:47:27 -0300

    adaptable para iphone

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "checklist-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./estilo.css",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
